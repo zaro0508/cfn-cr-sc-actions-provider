c2cef7854cd33fa995434c3c6e8fd25a8d6a6ebe
Khai Do <3697686+zaro0508@users.noreply.github.com>
2020-05-04 17:29:59 -0700

    [SC-26] Bastard custom handler (#7)
    This bastard custom handler was implemented to work around
    AWS issues that prevent the proper implementation to not work.
    
    This workaround was inspired by @tthyer and is described in
    the README file.

--- sc-actions-provider/associate.py
+++ sc-actions-provider/associate.py
@@ -18,13 +18,27 @@ def get_parameters(event):
     aws_account_id = event['StackId'].split(':')[4]
     service_action_id = event['ResourceProperties']['ServiceActionId']
     product_id = event['ResourceProperties']['ProductId']
-    provisioning_artifact_ids = event['ResourceProperties']['ProvisioningArtifactIds'].split("|")
-    return aws_account_id, service_action_id, product_id, provisioning_artifact_ids
+    return aws_account_id, service_action_id, product_id
 
-def create_service_action_associations(service_action_id, product_id, provisioning_artifact_ids):
+def get_provisioning_artifact_ids(product_id):
+    '''
+    Get all product version IDs
+    '''
+    response = sc.list_provisioning_artifacts(
+        ProductId=product_id
+    )
+    provisioning_artifact_details = response['ProvisioningArtifactDetails']
+    provisioning_artifact_ids = []
+    for provisioning_artifact_detail in provisioning_artifact_details:
+        provisioning_artifact_ids.append(provisioning_artifact_detail['Id'])
+
+    return provisioning_artifact_ids
+
+def create_service_action_associations(service_action_id, product_id):
     '''
     Create a list of service action associations for batch execution
     '''
+    provisioning_artifact_ids = get_provisioning_artifact_ids(product_id)
     service_action_assocations = []
     for provisioning_artifact_id in provisioning_artifact_ids:
         service_action_assocation = {
@@ -36,87 +50,42 @@ def create_service_action_associations(service_action_id, product_id, provisioni
 
     return service_action_assocations
 
-def associate_actions(aws_account_id, service_action_id, product_id, provisioning_artifact_ids):
+def associate_actions(aws_account_id, service_action_id, product_id):
     '''
     associate SC service actions with a product version
     '''
-
+    provisioning_artifact_ids = get_provisioning_artifact_ids(product_id)
     logger.debug(
         "Associate action " + service_action_id +
         " with product " + product_id + ", provisioning_artifact_ids: " +
         str(provisioning_artifact_ids))
     service_action_associations = create_service_action_associations(
         service_action_id,
-        product_id,
-        provisioning_artifact_ids)
+        product_id
+    )
     response = sc.batch_associate_service_action_with_provisioning_artifact(
         ServiceActionAssociations=service_action_associations
     )
     physical_resource_id = 'ass-{}'.format(str(uuid.uuid4()).replace('-', '')[0:13])
     return physical_resource_id
 
-def disassociate_actions(aws_account_id, service_action_id, product_id, provisioning_artifact_ids):
+def disassociate_actions(aws_account_id, service_action_id, product_id):
     '''
     disassociate SC service actions from a product version
     '''
+    provisioning_artifact_ids = get_provisioning_artifact_ids(product_id)
     logger.debug(
         "dis-associate action " + service_action_id +
         " with product " + product_id + ", provisioning_artifact_ids: " +
         str(provisioning_artifact_ids))
     associations = create_service_action_associations(
         service_action_id,
-        product_id,
-        provisioning_artifact_ids)
+        product_id
+    )
     response = sc.batch_disassociate_service_action_from_provisioning_artifact(
         ServiceActionAssociations=associations
     )
 
-def update_action_associations(event):
-    '''
-    Update SC service action associations
-
-    Adding a Product version (i.e. ProvisioningArtifactVersion) is an update event and
-    associating an action to a product depends on the ProvisioningArtifactVersion(s)
-
-    Adding a ProvisioningArtifactVersion works because CFN will create a new
-    ProvisioningArtifactVersion ID and pass it to the association custom resource.
-    The custom resource then associates the action with the new
-    ProvisioningArtifactVersion(s)
-
-    Removing a ProvisioningArtifactVersion does not work because AWS does not allow
-    deleting a ProvisioningArtifactVersion when there is an action associated with it.
-    The action would need to be disassociated from the ProvisioningArtifactVersion(s) before
-    the ProvisioningArtifactVersion can be removed.  This would only work if the dependency
-    is reversed (i.e. Provisioning versions depends on association)
-    '''
-
-    properties = event['ResourceProperties']
-    service_action_id = properties['ServiceActionId']
-    product_id = properties['ProductId']
-    provisioning_artifact_ids = properties['ProvisioningArtifactIds'].split("|")
-    old_properties = event['OldResourceProperties']
-    old_provisioning_artifact_ids = old_properties['ProvisioningArtifactIds'].split("|")
-
-    # We only associate new ProvisioningArtifactVersion(s)
-    new_provisioning_artifact_ids = []
-    if len(provisioning_artifact_ids) > len(old_provisioning_artifact_ids):
-        for curr_provisioning_artifact_id in provisioning_artifact_ids:
-            if curr_provisioning_artifact_id not in old_provisioning_artifact_ids:
-                new_provisioning_artifact_ids.append(curr_provisioning_artifact_id)
-
-        service_action_associations = create_service_action_associations(
-            service_action_id,
-            product_id,
-            new_provisioning_artifact_ids)
-        logger.debug(
-            "Associate action " + service_action_id +
-            " with product " + product_id + ", provisioning_artifact_ids: " +
-            str(new_provisioning_artifact_ids))
-        response = sc.batch_associate_service_action_with_provisioning_artifact(
-            ServiceActionAssociations=service_action_associations
-        )
-
-    return event['PhysicalResourceId']
 
 @helper.create
 def create(event, context):
@@ -126,7 +95,7 @@ def create(event, context):
 @helper.update
 def update(event, context):
     logger.debug("Received event: " + json.dumps(event, sort_keys=False))
-    return update_action_associations(event)
+    return event['PhysicalResourceId']
 
 
 @helper.delete
